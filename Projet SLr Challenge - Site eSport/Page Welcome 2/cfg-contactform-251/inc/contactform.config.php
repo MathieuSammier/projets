b30469cf2660a139980b8d27e6c4f575cea5ebcb
MathieuSammier <110030412+MathieuSammier@users.noreply.github.com>
2022-07-26 13:13:37 +0200

    Add files via upload

---
+++ Projet SLr Challenge - Site eSport/Page Welcome 2/cfg-contactform-251/inc/contactform.config.php
@@ -0,0 +1,19 @@
+<?php
+$cfg['email_address'] = 'admin@slr-challenge.fr';
+$cfg['email_from'] = 'Admin SLr-Challenge';
+$cfg['email_address_cc'] = '';
+$cfg['email_address_bcc'] = 'slrmats00@gmail.com';
+$cfg['timezone'] = 'Europe/Berlin';
+$cfg['form_name'] = 'ContactForm-Welcome-SLr';
+$cfg['form_validationmessage'] = 'Votre message a été envoyé.<br />Nous vous enverrons une réponse dans le plus court délai.';
+$cfg['form_errormessage_captcha'] = '';
+$cfg['form_errormessage_emptyfield'] = 'Ce champ ne peut pas être laissé vide !';
+$cfg['form_errormessage_invalidemailaddress'] = 'Votre adresse e-mail est invalide !';
+$cfg['form_redirecturl'] = '';
+$cfg['adminnotification_subject'] = 'Message reçu depuis le formulaire de contact - Welcome SLr';
+$cfg['usernotification_inputid'] = 'cfg-element-251-3';
+$cfg['usernotification_insertformdata'] = 1;
+$cfg['usernotification_format'] = 'plaintext';
+$cfg['usernotification_subject'] = 'Merci pour votre message !';
+$cfg['usernotification_message'] = 'Nous vous répondrons dès que possible !';
+?>
---
+++ Projet SLr Challenge - Site eSport/Page Welcome 2/contact/contact.php
@@ -0,0 +1,120 @@
+<?php
+/*
+	********************************************************************************************
+	CONFIGURATION
+	********************************************************************************************
+*/
+// Destinataire 
+$destinataire = 'admin@slr-challenge.fr';
+ 
+// Copie du mail
+$copie = 'oui';
+ 
+// Action du formulaire 
+$form_action = 'index.html#contact';
+ 
+/*
+	********************************************************************************************
+	FIN DE LA CONFIGURATION
+	********************************************************************************************
+*/
+ 
+/*
+ * Nettoyage et Enregistrement
+ */
+function Rec($text)
+{
+	$text = htmlspecialchars(trim($text), ENT_QUOTES);
+	if (1 === get_magic_quotes_gpc())
+	{
+		$text = stripslashes($text);
+	}
+ 
+	$text = nl2br($text);
+	return $text;
+};
+ 
+/*
+ * Vérification de la syntaxe
+ */
+function IsEmail($email)
+{
+	$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
+	return (($value === 0) || ($value === false)) ? false : true;
+}
+ 
+// Récupèration des champs.
+$nom     = (isset($_POST['name']))     ? Rec($_POST['name'])     : '';
+$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
+$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
+ 
+// Vérification des variable
+$email = (IsEmail($email)) ? $email : ''; // Vide = erroné ou alors non vide = correct
+$err_formulaire = false; // Remplir le formulaire en cas d'erreur
+ 
+if (isset($_POST['Submit']))
+{
+	if (($nom != '') && ($email != '') && ($message != ''))
+	{
+		// Envoyer le mail
+		$headers  = 'From:'.$nom.' <'.$email.'>' . "\r\n";
+		//$headers .= 'Reply-To: '.$email. "\r\n" ;
+		//$headers .= 'X-Mailer:PHP/'.phpversion();
+ 
+		// Copie au visiteur
+		if ($copie == 'oui')
+		{
+			$cible = $destinataire.','.$email;
+		}
+		else
+		{
+			$cible = $destinataire;
+		};
+ 
+		// Remplacement des caractères spéciaux
+		$message = str_replace("&#039;","'",$message);
+		$message = str_replace("&#8217;","'",$message);
+		$message = str_replace("&quot;",'"',$message);
+		$message = str_replace('&lt;br&gt;','',$message);
+		$message = str_replace('&lt;br /&gt;','',$message);
+		$message = str_replace("&lt;","&lt;",$message);
+		$message = str_replace("&gt;","&gt;",$message);
+		$message = str_replace("&amp;","&",$message);
+ 
+		// Envoi du mail
+		if (mail($cible, $message, $headers))
+		{
+			echo '<p>'.$message_envoye.'</p>';
+		}
+		else
+		{
+			echo '<p>'.$message_non_envoye.'</p>';
+		};
+	}
+	else
+	{
+		// Si 3 variables sont vides ...
+		echo '<p>'.$message_formulaire_invalide.'</p>';
+		$err_formulaire = true;
+	};
+}; // fin du if (!isset($_POST['envoi']))
+ 
+if (($err_formulaire) || (!isset($_POST['Submit'])))
+{
+	// Afficher le formulaire
+	echo '
+	<form id="contact" method="post" action="'.$form_action.'">
+	<fieldset><legend>Vos coordonnées</legend>
+		<p><label for="nom">Nom :</label><input type="text" id="nom" name="nom" value="'.stripslashes($nom).'" tabindex="1" /></p>
+		<p><label for="email">Email :</label><input type="text" id="email" name="email" value="'.stripslashes($email).'" tabindex="2" /></p>
+	</fieldset>
+ 
+	<fieldset><legend>Votre message :</legend>
+		<p><label for="objet">Objet :</label><input type="text" id="objet" name="objet" value="'.stripslashes($objet).'" tabindex="3" /></p>
+		<p><label for="message">Message :</label><textarea id="message" name="message" tabindex="4" cols="30" rows="8">'.stripslashes($message).'</textarea></p>
+	</fieldset>
+ 
+	<div style="text-align:center;"><input type="submit" name="envoi" value="Envoyer le formulaire !" /></div>
+	</form>';
+};
+?>
