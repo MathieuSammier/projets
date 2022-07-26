e8b994267685fec1f3ce66eb6fe953f24bb7503f
MathieuSammier <110030412+MathieuSammier@users.noreply.github.com>
2022-07-26 13:03:42 +0200

    Add files via upload

---
+++ Projet SpeedPermis - Site pour obtenir son permis rapidement/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+	<div class="container">
+  <!-- Content here -->
+  <a>test</a>
+  <div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+</div>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="/js/bootstrap.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+	<script src="/js/bootstrap.bundle.js"></script>
+	<script src="/js/bootstrap.bundle.min.js"></script>
+    <script link rel="stylesheet" href="/css/bootstrap.css"></script>
+	<script link rel="stylesheet" href="/css/bootstrap.min.css"></script>
+	<script link rel="stylesheet" href="/css/bootstrap-grid.css"></script>
+	<script link rel="stylesheet" href="/css/bootstrap-grid.min.css"></script>
+	<script link rel="stylesheet" href="/css/bootstrap-reboot.css"></script>
+	<script link rel="stylesheet" href="/css/bootstrap-reboot.min.css"></script>
+  </body>
+</html>
